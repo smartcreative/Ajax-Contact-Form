ccc7582cbabd513f63c5c0ee806d1762eff1d33e
Smart Creative <costi@smartcreative.ro>
2011-10-22 19:07:48 +0300

    third commit

---
+++ sc-code.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($){
+	$('#sc-contactform').submit(function(){
+		var action = $(this).attr('action');
+		$("#message").slideUp(750,function() {
+			$('#message').hide();
+			$('#sc-submit')
+				.after('<img src="ajax-loader.gif" class="loader" />')
+				.attr('disabled','disabled');
+			$.post(action, $("#sc-contactform").serialize(),
+				function(data){
+					document.getElementById('message').innerHTML = data;
+					$('#message').slideDown('slow');
+					$('#sc-contactform img.loader').fadeOut('slow',function(){$(this).remove()});
+					$('#sc-contactform #sc-submit').removeAttr('disabled'); 
+					if(data.match('success') != null) $('#sc-contactform').slideUp('slow');
+				}
+			);
+		});
+		return false;
+	});
+});
---
+++ sc-contact.php
@@ -0,0 +1,39 @@
+<?php 
+// Can be a custom message
+$error = "";
+
+if(!filter_var(trim($_REQUEST['sc-email']), FILTER_VALIDATE_EMAIL)) {
+    $error .= "Please enter a valid email address.";
+	$error .= "<br />";
+}
+if($_REQUEST['sc-verify'] != '4') {
+	$error .= "Wrong verify code.";
+	$error .= "<br />";
+}
+
+if($error == "") { 
+	
+	$domain = $_SERVER['SERVER_NAME'];
+	$to      = 'your@email.com'; // your contact email address
+	$subject = $_REQUEST['sc-subject'];
+	$from = "Name: ".trim($_REQUEST['sc-name'])." \n";
+	$from_email = "Email: ".trim($_REQUEST['sc-email'])." \n";
+	$message = "Message: ".trim($_REQUEST['sc-comments'])." \n";
+	$body .= $from;
+	$body .= $from_email;
+	$body .= $message;
+	
+	$headers = "Infos: $domain Contact Form \n";
+	
+	if(mail($to, $subject, $body, $headers)) { 
+		echo "Email sent successfully.<br />Thank you , your message has been submitted to us."; 
+	} 
+	else {
+		echo "Error sending email";
+	}
+
+} 
+else {
+	echo $error;
+}
+?>
